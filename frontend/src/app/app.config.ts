87bb0ecc403f0723787f4422d99897c0951221a4
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2026-02-11 14:33:26 +0100

    feat: landing page

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,5 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    // provideAppInitializer(() => {
+    //   const iconRegistry = inject(MatIconRegistry);
+    //   console.log(
+    //     'Setting default font set class for Material Icons',
+    //     iconRegistry.getDefaultFontSetClass(),
+    //   );
+    // iconRegistry.setDefaultFontSetClass('Material-Icons');
+    // }),
+    // { provide: MAT_ICON_DEFAULT_OPTIONS, useValue: { fontSet: 'Material Icons' } },
+  ],
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: 'dashboard',
+    title: 'Dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
+  {
+    path: 'table',
+    title: 'Table',
+    loadComponent: () => import('./table/table.component').then((m) => m.TableComponent),
+  },
+];
+
+export const routes: Routes = [
+  {
+    path: 'app',
+    title: 'App',
+    loadComponent: () =>
+      import('./navigation/navigation.component').then((m) => m.NavigationComponent),
+    children: appRoutes,
+  },
+  {
+    path: '',
+    title: 'BioIS',
+    loadComponent: () =>
+      import('./landing-page/landing-page.component').then((m) => m.LandingPageComponent),
+  },
+];
