c8691807791826ea91f8191732e3aea2b19d7f00
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2026-03-16 11:30:25 +0100

    feat: mock process for natura2000 data

--- backend/src/processes/mod.rs
+++ backend/src/processes/mod.rs
@@ -1,5 +1,8 @@
+mod habitat_distance;
 mod ndvi;
+mod parameters;
 mod path_info;
 
+pub use habitat_distance::HabitatDistanceProcess;
 pub use ndvi::NDVIProcess;
 pub use path_info::ProcessesOpenApiSpec;
--- backend/src/processes/ndvi.rs
+++ backend/src/processes/ndvi.rs
@@ -9,8 +9,8 @@ use geoengine_openapi_client::{
         GdalSource, GdalSourceParameters, GeoJson, Measurement, MockPointSource,
         MockPointSourceParameters, Names, RasterBandDescriptor, RasterDataType, RasterOperator,
         RasterVectorJoin, RasterVectorJoinParameters, SingleRasterSource,
-        SingleVectorMultipleRasterSources, SpatialPartition2D, TemporalAggregationMethod,
-        VectorOperator, WfsRequest, WfsService,
+        SingleVectorMultipleRasterSources, TemporalAggregationMethod, VectorOperator, WfsRequest,
+        WfsService,
     },
 };
 use ogcapi::{
@@ -32,6 +32,7 @@ use utoipa::ToSchema;
 
 use crate::{
     config::CONFIG,
+    processes::parameters::{Coordinate, PointGeoJsonInput, ToBbox},
     state::USER,
     util::{error_response, to_api_workflow},
 };
@@ -47,52 +48,6 @@ pub struct NDVIProcessInputs {
     pub year: Year,
     #[schema(minimum = 1, maximum = 6)]
     pub month: Month,
-}
-
-#[derive(Deserialize, Serialize, Debug, JsonSchema, ToSchema)]
-#[serde(transparent)]
-pub struct Coordinate(pub [f64; 2]);
-
-trait ToBbox {
-    fn to_bbox(&self, buffer: f64) -> SpatialPartition2D;
-}
-
-impl ToBbox for Coordinate {
-    fn to_bbox(&self, buffer: f64) -> SpatialPartition2D {
-        use geoengine_openapi_client::models::Coordinate2D;
-
-        let [x, y] = self.0;
-        SpatialPartition2D::new(
-            Coordinate2D::new(x + buffer, y - buffer),
-            Coordinate2D::new(x - buffer, y + buffer),
-        )
-    }
-}
-
-#[derive(Deserialize, Serialize, Debug, JsonSchema, ToSchema)]
-#[serde(rename_all = "camelCase")]
-pub struct PointGeoJsonInput {
-    pub value: PointGeoJson,
-    pub media_type: PointGeoJsonInputMediaType,
-}
-
-#[derive(Deserialize, Serialize, Debug, JsonSchema, ToSchema)]
-#[serde(rename_all = "camelCase")]
-pub enum PointGeoJsonInputMediaType {
-    #[serde(rename = "application/geo+json")]
-    GeoJson,
-}
-
-#[derive(Deserialize, Serialize, Debug, JsonSchema, ToSchema)]
-#[serde(rename_all = "camelCase")]
-pub struct PointGeoJson {
-    pub r#type: PointGeoJsonType,
-    pub coordinates: Coordinate,
-}
-
-#[derive(Deserialize, Serialize, Debug, JsonSchema, ToSchema)]
-pub enum PointGeoJsonType {
-    Point,
 }
 
 #[derive(Deserialize, Serialize, Debug, JsonSchema, ToSchema, Copy, Clone)]
